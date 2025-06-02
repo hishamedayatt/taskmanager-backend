69fc29e86f0e104c88c8b0318c0cb4fa7cfe2c0d
hishamedayatt <103420977+hishamedayatt@users.noreply.github.com>
2025-06-02 10:59:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ async function startServer() {
     const PORT = process.env.PORT||5000;
     try {
         await mongoose.connect(process.env.mongo_db);
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
         console.log(`connected db as port ${PORT}`);
         
     } catch (error) {
