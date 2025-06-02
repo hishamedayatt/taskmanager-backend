4c5a4ea3a84a9d2bcc62e6de282d3dd80dd90eaf
hishamedayatt <mohammedhisham859@gmail.com>
2025-06-02 10:08:02 +0530

    second commit

--- server.js
+++ server.js
@@ -1,66 +1,4 @@
-// import {ApolloServer} from '@apollo/server';
-// import { startStandaloneServer } from '@apollo/server/standalone';
-// import { gql } from 'graphql-tag';
 
-
-// const users = [
-//     {id:"1",title:"gdygfui", discription:"bjhbhbhb", status:"inprogress", date:"01/11/5000"},
-//     {id:"2",title:"gdygfui", discription:"bjhbhbhb", status:"compleate", date:"01/11/3000"},
-//     {id:"3",title:"gdygfui", discription:"bjhbhbhb", status:"incomleate", date:"01/11/1000"},
-//     {id:"4",title:"gdygfui", discription:"bjhbhbhb", status:"progress", date:"01/11/2000"}
-// ];
-
-// const typeDefs = gql `
-//     type Query {
-//         getUsers: [User]
-//         getUserById(id: ID): User
-//     }
-
-//     type Mutation {
-//         createUser(title: String!, discription: String!, status: String!, date: String!): User
-//     }
-
-//     type User {
-//         id: ID!
-//         title: String!
-//         discription: String!
-//         status: String!
-//         date: String!
-
-//     }`;
-
-// const resolvers = {
-//     Query: {
-//         getUsers: () =>{
-//            return users; 
-//         },
-//         getUserById: (parent,args) =>{
-//             const id = args.id
-//             return users.find((user) => user.id ===id)
-//         },
-//     },
-//     Mutation: {
-//         createUser: (parent,args) =>{
-//              const { title, discription, status, date } = args;
-//              const newUser = {
-//                 id: (users.length + 1).toString(),
-//                 title,
-//                 discription,
-//                 status,
-//                 date,
-//              };
-//              users.push(newUser);
-//         },
-       
-
-//     },
-// };
-// const server = new ApolloServer ({ typeDefs, resolvers });
-
-// const{url} = await startStandaloneServer(server, {
-//     listen:{port:4000},
-// });
-// console.log(`server running at ${url}`);
 
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
@@ -105,7 +43,7 @@ async function startServer() {
     }
     
     app.listen(PORT,()=>{
-        console.log(`express server running port ${PORT}`);
+        console.log(`express server running port http://localhost:${PORT}/graphql`);
         
     })
 }
